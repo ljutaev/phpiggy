b6e4d7e24da93ae35287fbccfcf801b75a215d89
Sasha <ljutaev@gmai.com>
2024-06-18 19:05:13 +0300

    fisrt commit

---
+++ src/Framework/App.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Framework;
+
+class App
+{
+    private Router $router;
+
+    public function __construct()
+    {
+        $this->router = new Router();
+    }
+
+    public function run(): void
+    {
+        echo 'Hello, World!';
+    }
+}
