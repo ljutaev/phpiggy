2e23840cd366b53a1a4dba5fe6f3ab0baf000a7c
Sasha <ljutaev@gmai.com>
2024-06-21 19:57:53 +0300

    router ready

--- public/index.php
+++ public/index.php
@@ -6,4 +6,4 @@ $app = include __DIR__ . '/../src/App/bootstrap.php';
 
 $app->run();
 
-dd($app);
+//dd($app);
--- src/Framework/App.php
+++ src/Framework/App.php
@@ -15,6 +15,14 @@ class App
 
     public function run(): void
     {
-        echo 'Hello, World!';
+        $path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+        $method = $_SERVER['REQUEST_METHOD'];
+
+        $this->router->dispatch($path, $method);
+    }
+
+    public function get(string $path, array $controller): void
+    {
+        $this->router->add('GET', $path, $controller);
     }
 }
